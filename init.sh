ace9ee0d3e04b95e4e2b1dbb36e0db0e18e1b57a
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:30:00 +0800

    use pip3 instead of pip

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 SCRIPT_PATH=$(pwd)
 ${SCRIPT_PATH}/init_submodule.sh python_utils git@github.com:ChenZaichuang/Python-Utils.git
-pip install -r ${SCRIPT_PATH}/requirements.txt
+pip3 install -r ${SCRIPT_PATH}/requirements.txt
 find ${SCRIPT_PATH} -type d -empty -delete

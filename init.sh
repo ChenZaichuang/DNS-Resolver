a210e99e5ac6d60cadf0876a4285312026eac735
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:49:17 +0800

    fix SCRIPT_PATH value

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-SCRIPT_PATH=$(pwd)
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ${SCRIPT_PATH}/init_submodule.sh python_utils git@github.com:ChenZaichuang/Python-Utils.git
 pip3 install -r ${SCRIPT_PATH}/requirements.txt
 find ${SCRIPT_PATH} -type d -empty -delete

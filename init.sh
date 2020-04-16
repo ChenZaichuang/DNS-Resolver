81edffb629331aeca4fbe7f85fb6a48a9e46ed4c
Chen Zaichuang <1158400745@qq.com>
2020-04-16 22:50:49 +0800

    Fix bash scripts

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-git rm --cached python_utils
-rm -r python_utils
-git submodule add git@github.com:ChenZaichuang/Python-Utils.git python_utils
-pip install -r python_utils/requirements.txt
+SCRIPT_PATH=$(pwd)
+${SCRIPT_PATH}/init_submodule.sh python_utils git@github.com:ChenZaichuang/Python-Utils.git
+pip install -r ${SCRIPT_PATH}/requirements.txt
